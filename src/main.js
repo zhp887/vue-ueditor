bebb9f9247e790c24ae1ab0dfe6e947e4ffa492e
zhp887 <1779160584@qq.com>
2020-08-10 21:55:20 +0800

    de

---
+++ src/main.js
@@ -0,0 +1,20 @@
+
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import App from './App'
+import router from './router'
+
+import '../static/UE/ueditor.config.js'
+import '../static/UE/ueditor.all.min.js'
+import '../static/UE/lang/zh-cn/zh-cn.js'
+import '../static/UE/ueditor.parse.min.js'
+
+Vue.config.productionTip = false
+Vue.use(VueRouter)
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  components: { App },
+  template: '<App/>'
+})
---
+++ src/views/editor/editor.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="components-container">
+    <button @click="getUEContent()">获取内容</button>
+    <button @click="getUEContentTxt()">获取无文本内容</button>
+    <div class="editor-container">
+      <UE :defaultMsg=defaultMsg :config=config :id=ue1 ref="ue"></UE>
+    </div>
+  </div>
+</template>
+<style>
+  .info{
+    border-radius: 10px;
+    line-height: 20px;
+    padding: 10px;
+    margin: 10px;
+    background-color: #ffffff;
+  }
+</style>
+<script>
+/* eslint-disable */
+import UE from '../../components/ue/ue.vue'
+export default {
+components: {UE},
+data() {
+    return {
+    defaultMsg: '',
+    config: {
+        initialFrameWidth: null,
+        initialFrameHeight: 350
+    },
+    ue1: "ue1", // 不同编辑器必须不同的id
+    ue2: "ue2"
+    }
+},
+methods: {
+    getUEContent() {
+    let content = this.$refs.ue.getUEContent()// 调用子组件方法
+    this.$notify({
+        title: '获取成功，可在控制台查看！',
+        message: content,
+        type: 'success'
+    })
+    console.log(content)
+    },
+    getUEContentTxt() {
+    let content = this.$refs.ue.getUEContentTxt()// 调用子组件方法
+    this.$notify({
+        title: '获取成功，可在控制台查看！',
+        message: content,
+        type: 'success'
+    });
+    console.log(content)
+    }
+  }
+}
+/* eslint-disable */
+</script>
+
+
+
